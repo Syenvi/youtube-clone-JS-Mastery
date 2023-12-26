d9088c72724e267d7cd364a13b059dab9d02a646
Syahdinrh <ryujinkun34@gmail.com>
2023-12-26 11:42:34 +0700

    search&video_detail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -34,10 +34,14 @@ const ChannelDetail = () => {
         />
         <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
       </Box>
-      <Box display={"flex"} p="2">
+      <Box
+        display={"flex"}
+        p="2"
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
         <Box
           sx={{
-            mr: { sm: "100px" },
+            px: "100px",
           }}
         >
           <Videos videos={videos} />
